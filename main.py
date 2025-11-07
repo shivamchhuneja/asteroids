024bbdf83598c74218657f2ebe01bf87f7106c6d
Shivam <shivam@brandsutra.org>
2025-11-07 13:18:40 +0530

    minor update to main file to log screen size properly

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import pygame
 from constants import *
 from logger import log_state
 
-pygame.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     BLACK = (48, 25, 52)
 
